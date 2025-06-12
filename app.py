2583c26ea761461cb0380df9399d317f7cdbce5c
Rugvedrc <144464437+Rugvedrc@users.noreply.github.com>
2025-06-13 00:32:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -778,4 +778,5 @@ def compare_models():
         return jsonify({'success': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
